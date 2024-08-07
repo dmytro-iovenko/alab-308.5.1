235a43982fc4e8cd238dacaedf3017244a62a7a3
Dmytro Iovenko <iovenko@gmail.com>
2024-08-06 21:34:00 -0400

    add 'avg' function

--- index.js
+++ index.js
@@ -2,9 +2,20 @@
 
 //Take an array of numbers and return the sum.
 const sum = (arr) => {
+  if (!Array.isArray(arr) || !arr.length) return 0;
   let sum = 0;
   arr.forEach((item) => (sum += item));
   return sum;
 };
 
 console.log(sum([1, 2, 3, 4, 5]));
+
+//Take an array of numbers and return the average.
+const avg = (arr) => {
+  if (!Array.isArray(arr) || !arr.length) return 0;
+  let sum = 0;
+  arr.forEach((item) => (sum += item));
+  return sum / arr.length;
+};
+
+console.log(avg([1, 2, 3, 4, 5]));
